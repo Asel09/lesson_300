0e1c764a96ae4ed4cc4c4f5142d0563d58a202fb
Asel <asel.ergeshova2001@gmail.com>
2020-11-14 21:38:18 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static int globalNumber = 6;
+
+    public static void main(String[] args) {
+        //       int num =10;
+        //       System.out.println(globalNumber);
+//        secondMethod();
+
+        int[] array1 = {10, 5, 20};
+        System.out.println(array1[1]);
+
+    }
+
+}
+
